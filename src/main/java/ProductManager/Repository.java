a1b6526ad6396669801a29cd81e50d04f3dd37d1
ksenia <ksy184694@gmail.com>
2023-03-20 22:20:52 +0300

    adding NotFoundException

--- src/main/java/ProductManager/Repository.java
+++ src/main/java/ProductManager/Repository.java
@@ -17,8 +17,12 @@ public class Repository {
     }
 
     public void removeById(int removeId) {
-        Product[] tmp = new Product[products.length - 1];
+        Product removeProduct = findById(removeId);
+        if (removeProduct == null) {
+            throw new NotFoundException(removeId);
+        }
 
+        Product[] tmp = new Product[products.length - 1];
         int index = 0;
         for (Product product : products) {
             if (product.getId() != removeId) {
@@ -27,5 +31,14 @@ public class Repository {
             }
         }
         products = tmp;
+    }
+
+    public Product findById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
     }
 }
--- src/test/java/ProductManager/RepositoryTest.java
+++ src/test/java/ProductManager/RepositoryTest.java
@@ -31,4 +31,26 @@ class RepositoryTest {
 
     }
 
+    @Test
+    public void testRemoveProductIfNotExist() {
+        Repository repo = new Repository();
+
+        Book book1 = new Book(10, "Gone with the wind", 1000, "Margaret Mitchell");
+        Book book2 = new Book(257, "Pride and Prejudice", 900, "Jane Austen");
+        Book book3 = new Book(333, "The Mysterious Affair at Styles", 100, "Agatha Christie");
+        Book book4 = new Book(47, "Mysterious", 100, "Mike Gordan");
+        Smartphone smartphone1 = new Smartphone(5466, "Iphone 13 Pro", 86_000, "Apple");
+
+        repo.add(book1);
+        repo.add(book2);
+        repo.add(book3);
+        repo.add(book4);
+        repo.add(smartphone1);
+
+        Assertions.assertThrows(NotFoundException.class,
+                () -> repo.removeById(100)
+        );
+
+    }
+
 }
